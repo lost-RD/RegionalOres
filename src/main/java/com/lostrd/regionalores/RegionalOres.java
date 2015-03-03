534403a6be9908f006129a101a1ccc8125199142
lost-RD <lost_RD@live.com>
2015-03-04 00:07:08 +1030

    let's mod reboot episode 3

---
+++ src/main/java/com/lostrd/regionalores/RegionalOres.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Lost_RD on 3/03/2015.
+ */
+
+package com.lostrd.regionalores;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="RegionalOres", name="Regional Ores", version="1.0")
+
+public class RegionalOres
+{
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
